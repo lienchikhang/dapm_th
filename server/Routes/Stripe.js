fd5c4a8b37b5e127cb14527962a2e7bb86273692
lienchikhang <lienchikhang777@gmail.com>
2023-10-16 15:19:33 +0700

    fix filter

--- server/Routes/Stripe.js
+++ server/Routes/Stripe.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const Stripe = require('stripe');
-const { makeOrderbyiduser } = require('../controllers/orderController');
-require('dotenv').config()
-const router = express.Router()
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
+const express = require("express");
+const Stripe = require("stripe");
+const { makeOrderbyiduser } = require("../controllers/orderController");
+require("dotenv").config();
+const router = express.Router();
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-router.post('/create-checkout-session', async (req, res) => {
-    try{
-        const line_items = req.body.shoes.map((shoe) => {
-            return {
-                price_data: {
-                    currency: 'vnd',
-                    product_data: {
-                        name: shoe.name,
-                        images:[shoe.img],
-                        metadata:{
-                            id:shoe._id
-                        }
-                    },
-                    unit_amount: shoe.price,
-                },
-                quantity: shoe.quantity,
-            }
-        })
-        const session = await stripe.checkout.sessions.create({
-            line_items: line_items,
-            mode: 'payment',
-            success_url: `${process.env.Success_url}`,
-            cancel_url: `${process.env.fail_url}`,
-        });
-        
-        res.send({ url: session.url });
-    }catch(err){
-        console.log(err)
-    }
+router.post("/create-checkout-session", async (req, res) => {
+  try {
+    const line_items = req.body.shoes.map((shoe) => {
+      return {
+        price_data: {
+          currency: "vnd",
+          product_data: {
+            name: shoe.name,
+            images: [shoe.img],
+            metadata: {
+              id: shoe._id,
+            },
+          },
+          unit_amount: shoe.price,
+        },
+        quantity: shoe.quantity,
+      };
+    });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${process.env.Success_url}`,
+      cancel_url: `${process.env.fail_url}`,
+    });
+
+    res.send({ url: session.url });
+  } catch (err) {
+    console.log(err);
+  }
 });
-module.exports = router
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,31 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const userRoute = require('./Routes/user');
-const shoeRoute = require('./Routes/shoe');
-const cartRoute = require('./Routes/cart');
-const cateRoute = require('./Routes/category');
-const orderRoute=require('./Routes/order')
-const stripeRoute=require("./Routes/Stripe")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoute = require("./Routes/user");
+const shoeRoute = require("./Routes/shoe");
+const cartRoute = require("./Routes/cart");
+const cateRoute = require("./Routes/category");
+const orderRoute = require("./Routes/order");
+const stripeRoute = require("./Routes/Stripe");
 const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
+
+//cho phép các request gửi lên ở dạng json
 app.use(express.json());
 
 //cho phép truy cập path của server từ các tên miền khác
 app.use(cors());
 
 /**connect mongodb */
-mongoose.connect(process.env.MONGO_DB)
-.then(()=>{console.log("connect successfully")})
-.catch((err) => console.log(err))
-  
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("connect successfully");
+  })
+  .catch((err) => console.log(err));
 
-app.use('/api/user/',userRoute)
-app.use('/api/shoe', shoeRoute)
-app.use('/api/cart', cartRoute)
-app.use('/api/category', cateRoute)
-app.use('/api/order',orderRoute)
-app.use('/api/stripe',stripeRoute)
+app.use("/api/user/", userRoute);
+app.use("/api/shoe", shoeRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/category", cateRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/stripe", stripeRoute);
 
 app.listen(process.env.PORT, () => console.log("server is running"));

6c8b560e367d4844230ce0bddd7bdd5d62053e76
lienchikhang <lienchikhang777@gmail.com>
2024-03-16 21:43:34 +0700

    mess:: adjust class services (factory method)

--- client/src/classes/Services.js
+++ client/src/classes/Services.js
@@ -1,31 +1,23 @@
 import Cart from "./Cart";
-import HttpSingleton from "./Http";
-import Shoe from "./Shoe";
 import User from "./User";
 
 //factory pattern
 export class Services {
-  service;
   #token = "";
   constructor(token) {
     this.#token = token;
   }
 
   createService(type) {
+    var service;
     switch (type) {
       case "cart": {
-        return (this.service = new Cart(type, this.#token));
-      }
-      case "checkout": {
-      }
-      case "order": {
-      }
-      case "shoe": {
-        return (this.service = new Shoe(type, this.#token));
+        service = new Cart(type, this.#token);
       }
       case "user": {
-        return (this.service = new User(type, this.#token));
+        service = new User(type, this.#token);
       }
     }
+    return service;
   }
 }
